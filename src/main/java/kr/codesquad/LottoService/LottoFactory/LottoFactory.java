c7ab32cc5e4a3997b1a49b9670be754fa027e246
Toa <ld980105@snu.ac.kr>
2023-01-04 08:56:14 +0900

    refactor : LottoFactory 내의 매직 넘버 상수화

--- src/main/java/kr/codesquad/LottoService/LottoFactory/LottoFactory.java
+++ src/main/java/kr/codesquad/LottoService/LottoFactory/LottoFactory.java
@@ -9,21 +9,23 @@ import java.util.List;
 public class LottoFactory{
     private List<Integer> numbers;
     private static final int LOTTO_SIZE = 6;
+    private static final int LOTTO_START_NUMBER = 1;
     private static final int LOTTO_LAST_NUMBER = 45;
-
+    private static final String AUTOMATIC = "automatic";
+    private static final String MANUAL = "manual";
     public LottoFactory() {
         numbers = new ArrayList<>();
-        for (int number = 1; number <= LOTTO_LAST_NUMBER; number++) {
+        for (int number = LOTTO_START_NUMBER; number <= LOTTO_LAST_NUMBER; number++) {
             numbers.add(number);
         }
     }
 
     public Lotto generateLotto(String type) {
-        if (type.equals("automatic")) {
+        if (type.equals(AUTOMATIC)) {
             Collections.shuffle(numbers);
             return new Lotto(new ArrayList<>(numbers.subList(0, LOTTO_SIZE)));
         }
-        if (type.equals("manual")) {
+        if (type.equals(MANUAL)) {
             return null;
         }
         throw new IllegalArgumentException("유효하지 않은 로또 종류입니다");
